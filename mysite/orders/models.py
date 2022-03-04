6c1cc93c2eaa94b95e92ef6d2a9ce6a01ad58d97
udinilya <udin76@bk.ru>
2022-03-04 14:52:25 +0300

    add forms

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -1,26 +1,36 @@
 from django.db import models
-from mysite.main.models import Product
 
 
 class Order(models.Model):
+    first_name = models.CharField('имя', max_length=50)
+    last_name = models.CharField('фамилия', max_length=50)
     email = models.EmailField()
+    address = models.CharField('адрес', max_length=250)
+    postal_code = models.CharField('индекс', max_length=20)
+    city = models.CharField('город', max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
 
     def __str__(self):
-        return 'Order {}'.format(self.id)
+        return str(self.id)
 
     class Meta:
         ordering = ('-created',)
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey('main.Product', on_delete=models.CASCADE, related_name='order_items')
     price = models.DecimalField('цена', max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField('количество', default=1)
 
     def __str__(self):
-        return '{}'.format(self.id)
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
--- mysite/orders/views.py
+++ mysite/orders/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import OrderItem
+from .forms import OrderCreateForm
 
-# Create your views here.
+
+def order_create(request):
+    product = get_object_or_404(OrderItem.product)
+    form = OrderCreateForm(request)
+    if form.is_valid():
+        order = form.save()
